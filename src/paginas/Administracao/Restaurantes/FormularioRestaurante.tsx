416a063d6f02f820d63a7264725671097a10fd67
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-08-22 14:23:11 -0300

    Editando restaurante

--- src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,19 +1,39 @@
 import { Button, TextField } from "@mui/material";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import IRestaurante from "../../../interfaces/IRestaurante";
 
 
 const FormularioRestaurante = () => {
   const [ nomeDoRestaurante, setNomeDoRestaurante ] = useState('');
+  const parametros = useParams();
+
+  useEffect(() => {
+    if(parametros.id) {
+      axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+        .then(response => setNomeDoRestaurante(response.data.nome))
+    }
+  }, [parametros])
 
   const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
     evento.preventDefault();
-    axios.post('http://localhost:8000/api/v2/restaurantes/', {
-      nome: nomeDoRestaurante
-    })  
-      .then(() => {
-        alert(`Restaurante ${nomeDoRestaurante} cadastrado com sucesso!`)
+
+    if(parametros.id) {
+      axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+        nome: nomeDoRestaurante
+      })
+        .then(() => {
+          alert(`Restaurante ${nomeDoRestaurante} alterado com sucesso!`)
+        })
+    } else {
+      axios.post('http://localhost:8000/api/v2/restaurantes/', {
+        nome: nomeDoRestaurante
+      })  
+        .then(() => {
+          alert(`Restaurante ${nomeDoRestaurante} cadastrado com sucesso!`)
       })    
+    }
   }
   return (
   <form onSubmit={aoSubmeterForm}>
@@ -21,7 +41,8 @@ const FormularioRestaurante = () => {
       value={nomeDoRestaurante}
       onChange={evento => setNomeDoRestaurante(evento.target.value)}
       label='Nome do restaurante' 
-      variant='standard' />
+      variant='standard' 
+    />
     <Button type="submit" variant='outlined'>Salvar</Button>
   </form>
   )
