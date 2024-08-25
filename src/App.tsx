dc28f4840e75f85191b1956b58927cea4afbf3fe
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-08-25 00:58:10 -0300

    Adicionando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './paginas/Home';
 import VitrineRestaurantes from './paginas/VitrineRestaurantes';
-import AdmistracaoRestaurantes from './paginas/Administracao/Restaurantes/AdmnistracaoRestaurantes';
+import AdmistracaoRestaurantes from './paginas/Administracao/Restaurantes/AdministracaoRestaurantes';
 import FormularioRestaurante from './paginas/Administracao/Restaurantes/FormularioRestaurante';
 import PaginaBaseAdministracao from './paginas/Administracao/PaginaBaseAdministracao';
+import AdministracaoPratos from './paginas/Administracao/Pratos/AdministracaoPratos';
+import FormularioPrato from './paginas/Administracao/Pratos/FormularioPrato';
 
 function App() {
 
@@ -16,6 +18,10 @@ function App() {
         <Route path="restaurantes" element={<AdmistracaoRestaurantes />} />
         <Route path="restaurantes/novo" element={<FormularioRestaurante />} />
         <Route path="restaurantes/:id" element={<FormularioRestaurante />} />
+        
+        <Route path="pratos/" element={<AdministracaoPratos />} />
+        <Route path="pratos/novo" element={<FormularioPrato />} />
+        <Route path="pratos/:id" element={<FormularioPrato />} />
       </Route>
     </Routes>
   );
