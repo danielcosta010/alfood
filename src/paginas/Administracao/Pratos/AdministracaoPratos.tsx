0c9109c120ed1118089eaee86e27946f8b0abb50
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-08-25 01:01:06 -0300

    Criando AdministracaoPratos.tsx

---
+++ src/paginas/Administracao/Pratos/AdministracaoPratos.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import IPrato from "../../../interfaces/IPrato";
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Link } from "react-router-dom";
+import http from "../../../http";
+
+
+const AdministracaoPratos = () => {
+  const [pratos, setPratos] = useState<IPrato[]>([]);
+
+  useEffect(() => {
+    http.get<IPrato[]>("pratos/")
+      .then((response) => setPratos(response.data));
+  }, []);
+
+  function excluir(pratoAhExcluir: IPrato) {
+    http.delete(`pratos/${pratoAhExcluir.id}/`)
+      .then(() => {
+        const listaDePratos = pratos.filter(Prato => Prato.id !== pratoAhExcluir.id)
+        setPratos([ ...listaDePratos ])
+      })
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Nome</TableCell>
+            <TableCell>Tag</TableCell>
+            <TableCell>Imagem</TableCell>
+            <TableCell>Editar</TableCell>
+            <TableCell>Excluir</TableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {pratos.map(prato => 
+            <TableRow key={prato.id}>
+              <TableCell >
+                {prato.nome}
+              </TableCell>
+              <TableCell >
+                {prato.tag}
+              </TableCell>
+              <TableCell >
+                [ <a href={prato.imagem} target="_blank" rel="noreferrer">Ver imagem</a> ]
+              </TableCell>
+              <TableCell >
+                [ <Link to={`/admin/pratos/${prato.id}`}>editar</Link> ]
+              </TableCell>
+              <TableCell >
+                <Button variant="outlined" color="error" onClick={() => excluir(prato)}>
+                  Excluir
+                </Button>
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default AdministracaoPratos;
