2a2e77a9fe51022fae9cadd6107162881d67f57d
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-08-25 01:00:42 -0300

    Atualizando PaginaBaseAdministracao

--- src/paginas/Administracao/PaginaBaseAdministracao.tsx
+++ src/paginas/Administracao/PaginaBaseAdministracao.tsx
@@ -26,6 +26,12 @@ const PaginaBaseAdministracao = () => {
               <Link component={RouterLink} to="/admin/restaurantes/novo">
                 <Button sx={{ my: 2, color: "white" }}>Novo Restaurante</Button>
               </Link>
+              <Link component={RouterLink} to="/admin/pratos">
+                <Button sx={{ my: 2, color: "white" }}>Pratos</Button>
+              </Link>
+              <Link component={RouterLink} to="/admin/pratos/novo">
+                <Button sx={{ my: 2, color: "white" }}>Novo Prato</Button>
+              </Link>
             </Box>
           </Toolbar>
         </Container>
